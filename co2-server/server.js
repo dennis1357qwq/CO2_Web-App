a63d82e6bd1d2fe6c545fe9e147baa31fdc0391e
Dennis Schreiber <boamah1357@gmail.com>
2023-05-28 21:57:21 +0200

    change listening port for server side

--- co2-server/server.js
+++ co2-server/server.js
@@ -5,6 +5,6 @@ app.get("/api", (req, res) => {
   res.json({ user: ["1", "2", "3"] });
 });
 
-app.listen(3001, () => {
-  console.log("server started on port 3001");
+app.listen(5002, () => {
+  console.log("server started on port 5002");
 });
