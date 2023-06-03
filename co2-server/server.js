6d928835cc19c0de80d76f619f365b6fbfdd5119
Dennis Schreiber <boamah1357@gmail.com>
2023-06-03 03:21:38 +0200

    specified Centers Array and added route for single center api get call

--- co2-server/server.js
+++ co2-server/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
 
-const centers = ["center 1", "center 2", "center 3", "center 4", "center 5"];
+const centers = [
+  { id: 1, location: "there...", peakConsumption: 100000, name: "center 1" },
+  {
+    id: 2,
+    location: "elsewhere...",
+    peakConsumption: 110000,
+    name: "center 2",
+  },
+  { id: 3, location: "there...", peakConsumption: 120000, name: "center 3" },
+];
 
 app.get("/api", (req, res) => {
   res.json({
     centers,
+  });
+});
+
+app.get("/api/center/:id", (req, res) => {
+  const center = centers.find((c) => c.id === parseInt(req.params.id));
+  if (!center) res.status(404);
+  res.json({
+    center: center,
   });
 });
 
