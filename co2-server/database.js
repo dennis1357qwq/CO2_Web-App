70ac05f6253a98c522c6361e77e01009e4efe896
julial79 <loetschert@tu-berlin.de>
2023-06-06 22:07:15 +0200

    first draft for user db queries

--- co2-server/database.js
+++ co2-server/database.js
@@ -52,3 +52,28 @@ export async function deleteCenter(id) {
 
   return getCenter(id);
 }
+
+
+export async function addUser(username, password) {
+  const [result] = await pool.query(
+    `
+    INSERT INTO users (username, password)
+    VALUES (?,?)
+    `,
+    [username, password]
+  );
+
+  const id = result.insertId;
+  return getUser(id);
+}
+
+export async function getUser(id) {
+  const [rows] = await pool.query(
+    `
+    SELECT * FROM users WHERE user_id = ?
+    `,
+    [id]
+  );
+
+  return rows[0];
+}
