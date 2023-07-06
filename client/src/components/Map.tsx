1dabcf0b148bf976e32b60cfea00f6f1e4b69f9d
Dennis Schreiber <boamah1357@gmail.com>
2023-07-06 07:12:56 +0200

    change db datatype of coordinates and show multiple points at once in overview

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,19 +1,18 @@
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
-import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { pointlist, mapPoint, CenterStack, CenterObj } from "./CenterInterface";
 
 export function Map(points: CenterStack) {
   return (
     <div className="MapWrapper">
-      <MapContainer center={[53, 13]} zoom={5}>
+      <MapContainer center={[52.521314, 13.361279]} zoom={12}>
         <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
         {points.centers.map(
           (point: CenterObj, index: number = point.center_id) => (
-            <Marker
-              key={index}
-              position={[point.lattitude, point.longitude]}
-            ></Marker>
+            <Marker key={index} position={[point.lattitude, point.longitude]}>
+              <Popup>{point.name}</Popup>
+            </Marker>
           )
         )}
       </MapContainer>
