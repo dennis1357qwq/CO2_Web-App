424498052f3c1703e949453a95d4ffd6ae31f681
justin <j.findeisen@campus.tu-berlin.de>
2023-07-20 15:35:24 +0200

    added ip to dennis server vm

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5002",
+      "/api": "http://35.232.166.210",
     },
   },
   plugins: [react()],
